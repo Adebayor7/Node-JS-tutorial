7ec91c12e71021b0da50ea04ac9d56320af57ab3
Adebayor <idrisadeebayorr12@gmail.com>
2021-12-22 21:25:21 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("Starting app.js");
+
+const fs = require("fs");
+const yargs = require("yargs");
+const notes = require("./notes.js");
+
+console.log("Process", process.argv);
+console.log("Yargs", yargs.argv);
